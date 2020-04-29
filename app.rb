ebdc679dc2731f075240822c0fc5f944574ca859
LTurns <lizzieturney@gmail.com>
2020-04-29 16:40:52 +0100

    Learning post - redirect - get

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
   get "/" do
     "Testing infrastructure working!"
   end
@@ -12,9 +13,15 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-  @player_1 = params[:player_1]
-  @player_2 = params[:player_2]
-  erb :play
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
+    erb :play
   end
 
 end
